6c7113d9b042f24adeb963af65bb47ce39f12abd
Shota Kashihara <mercury4th@gmail.com>
2021-01-30 22:14:19 +0900

    update

--- 9cc.c
+++ 9cc.c
@@ -5,6 +5,76 @@
 #include <stdlib.h>
 #include <string.h>
 
+// 抽象構文木のノードの種類
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+// 抽象構文木のノードの型
+struct Node {
+  NodeKind kind; // ノードの型
+  Node *lhs; // 左辺
+  Node *rhs; // 右辺
+  int val; // kind が ND_NUM の場合のみ使う
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *expr() {
+  Node *node = mul();
+
+  for (;;) {
+    if (consume('+'))
+      node = new_node(ND_ADD, node, mul());
+    else if (consume('-'))
+      node = new_node(ND_SUB, node, mul());
+    else
+      return node;
+  }
+}
+
+Node *mul() {
+  Node *node = primary();
+
+  for (;;) {
+    if (consume('*'))
+      node = new_node(ND_MUL, node, primary());
+    else if (consume('/'))
+      node = new_node(ND_DIV, node, primary());
+    else
+      return node;
+  }
+}
+
+Node *primary() {
+  // 次のトークンが "(" なら、"(" expr ")" のはず
+  if (consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+  return new_node_num(expect_number());
+}
+
 // トークンの種類
 typedef enum {
   TK_RESERVED, // 記号
@@ -131,6 +201,9 @@ int main(int argc, char **argv) {
   }
 
   user_input = argv[1];
+
+  Node *node = calloc(1, sizeof(Node));
+  node = expr();
 
   // トークナイズする
   token = tokenize();
